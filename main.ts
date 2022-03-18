2b2b6d731484b742722159c0e1c2f5cd6901ffb8
Sophia Xu <i@sophiaxu.me>
2022-03-18 15:38:50 -0400

    feat: obsidian sync first pass

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import { mountComponentWithUid, initUnigraphEmbed } from 'unigraph-dev-explorer/lib/index'
 
 // Remember to rename these classes and interfaces!
 
@@ -7,7 +8,38 @@ interface MyPluginSettings {
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	mySetting: 'localhost'
+}
+
+function parseNoteToMarkdown (note: any, heading: number = 1, parsed: any[] = []) {
+	const name = note.get('text').as('primitive');
+	parsed.push({
+		text: note.getType() === '$/schema/note_block' 
+			? name 
+			: `\`\`\`unigraph\n${note._value.content._value.uid}\n\`\`\``,
+		heading
+	});
+	(note.get('children')?.['_value['] || [])
+		.sort((a: any, b: any) => a?.['_index']?.['_value.#i'] - b?.['_index']?.['_value.#i'])
+		.map((child: any) => {
+			if (child?._value?.type?.['unigraph.id'] === '$/schema/subentity') {
+				console.log(note, child);
+				const childNote = note.__proto__.constructor(child?._value?._value);
+				parseNoteToMarkdown(childNote, heading + 1, parsed);
+		}
+	})
+}
+
+function parsePostProcess (parsed: any[]) {
+	let maxHeading = 1;
+	parsed.forEach(el => {
+		if (el.heading > maxHeading) maxHeading = el.heading;
+	})
+	return parsed.map(el => {
+		return el.heading < maxHeading && !el.text.startsWith('```unigraph\n') 
+			? `${'#'.repeat(el.heading)} ${el.text}` 
+			: el.text
+	}).join('\n')
 }
 
 export default class MyPlugin extends Plugin {
@@ -15,67 +47,71 @@ export default class MyPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
+		initUnigraphEmbed(this.settings.mySetting);
+		//console.log(mountComponentWithUid)
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		setTimeout(() => {
+			// Unigraph ready, now do things
+			(window as any).unigraph.startSyncListen('$/entity/obsidian_sync_resource', 'UnigraphTest');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+			(window as any).unigraph.backendConnection.current.addEventListener('message', (message: MessageEvent) => {
+				const data = message.data;
+				let parsed;
+				try {
+					parsed = JSON.parse(data);
+					if (parsed.type !== 'sync_updated') parsed = undefined;
+				} catch (e) {}
+				if (!parsed) return;
+				const uids: string[] = parsed.result;
+				// TODO: sync those things now
+				(window as any).unigraph.getObject(uids).then((objs: any[]) => {
+					Promise.all(objs.map(async (note: any) => {
+						const title = note.get('text').as('primitive');
+						const path = title + '.md';
+						const mkd: any[] = [];
+						parseNoteToMarkdown(note, 1, mkd);
+						const text = parsePostProcess(mkd);
+						try {
+							await this.app.vault.create(path, text)
+						} catch (e: any) {
+							if (e?.message.includes('File already exists')) {
+								const file = this.app.vault.getAbstractFileByPath(path);
+								await this.app.vault.modify(file as TFile, text);
+							}
+						}
+						return true;
+					})).then(() => {
+						(window as any).unigraph.acknowledgeSync('$/entity/obsidian_sync_resource', 'UnigraphTest', uids)
+					})
+				});
+			})
+		}, 2000)
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.registerMarkdownCodeBlockProcessor("unigraph", (source, el, ctx) => {
+			/*const rows = source.split("\n").filter((row) => row.length > 0);
+	  
+			const table = el.createEl("table");
+			const body = table.createEl("tbody");
+	  
+			for (let i = 0; i < rows.length; i++) {
+			  const cols = rows[i].split(",");
+	  
+			  const row = body.createEl("tr");
+	  
+			  for (let j = 0; j < cols.length; j++) {
+				row.createEl("td", { text: cols[j] });
+			  }
+			}*/
+			const div = el.createDiv();
+			div.id = "unigraph-entity-" + source;
+			console.log(div);
+			setTimeout(() => {
+				mountComponentWithUid(source, "unigraph-entity-" + source)
+			}, 0)
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -120,16 +156,15 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Hostname')
+			.setDesc('Hostname of the Unigraph server to sync with')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
+				.setPlaceholder('localhost')
 				.setValue(this.plugin.settings.mySetting)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
 					this.plugin.settings.mySetting = value;
 					await this.plugin.saveSettings();
 				}));
